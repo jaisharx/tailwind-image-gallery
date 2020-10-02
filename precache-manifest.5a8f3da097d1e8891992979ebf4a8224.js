4eabfc386beea06a2509a0575361f00b8e986b93
jaisharx <sendtojsharma@gmail.com>
2020-10-02 12:20:21 +0000

    Deploying to gh-pages from  @ 29d3be823fa7715512d09e30bfe661cce32e61ad 🚀

---
+++ precache-manifest.5a8f3da097d1e8891992979ebf4a8224.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f32596550169fa4a50576f4f45b9601",
+    "url": "/tailwind-image-gallery/index.html"
+  },
+  {
+    "revision": "26702a82f83fd98a441b",
+    "url": "/tailwind-image-gallery/static/css/main.fd6d9b2c.chunk.css"
+  },
+  {
+    "revision": "653a08e3e867b3e03a45",
+    "url": "/tailwind-image-gallery/static/js/2.f86f32da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tailwind-image-gallery/static/js/2.f86f32da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26702a82f83fd98a441b",
+    "url": "/tailwind-image-gallery/static/js/main.ebfc32ef.chunk.js"
+  },
+  {
+    "revision": "f1cfe1b06ac477edaaf5",
+    "url": "/tailwind-image-gallery/static/js/runtime-main.c352d264.js"
+  }
+]);
